97a0f7c3e0a4c51baf1a8f5b379338fb376dd6c6
Asad Tariq <asadtariq.at66@gmail.com>
2024-09-12 12:48:42 +0500

    add: user model added

---
+++ src/api/v1/models/user_model.py
@@ -0,0 +1,33 @@
+"""
+    This file has the User model
+    its attributes, constraints, and relationships
+"""
+
+from sqlalchemy import UUID, Column, VARCHAR, DATETIME, Text, BOOLEAN, TIMESTAMP, func
+
+# from sqlalchemy.orm import relaltionship
+
+from . import Base
+
+
+class User(Base):
+    """User Model
+    This class represents the user in the app
+    It contains information as user id, username, firstname
+    """
+
+    __tablename__ = "user"
+
+    user_id = Column(UUID(as_uuid=True), primary_key=True, index=True)
+    username = Column(VARCHAR(64), unique=True, nullable=False)
+    email = Column(VARCHAR(64), unique=True, nullable=False)
+    first_name = Column(VARCHAR(64), nullable=False)
+    last_name = Column(VARCHAR(64), nullable=False)
+    password = Column(VARCHAR(32))
+    dob = Column(DATETIME, nullable=False)
+    ver_key = Column(Text)
+    is_verified = Column(BOOLEAN, default=False)
+    is_active = Column(VARCHAR(32), default=True)
+    created_on = Column(TIMESTAMP, server_default=func.now())
+    last_modified = Column(TIMESTAMP, server_default=func.now())
+    profile_img_path = Column(Text, nullable=True)
