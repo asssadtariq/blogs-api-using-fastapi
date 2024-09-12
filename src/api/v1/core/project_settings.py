c8fe23f513eb7b50579c2b39444a4f7bb12837ee
Asad Tariq <asadtariq.at66@gmail.com>
2024-09-12 15:45:58 +0500

    add: db creds added in project settings

--- src/api/v1/core/project_settings.py
+++ src/api/v1/core/project_settings.py
@@ -3,6 +3,8 @@ It keeps project's constants variables
 variables imported from environment
 """
 
+import os
+from urllib import parse
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
@@ -11,4 +13,21 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    pass
+    """Class to represent project settings"""
+
+    ## Database details
+    DB_HOST: str = os.environ.get("DB_HOST")
+    DB_NAME: str = os.environ.get("DB_NAME")
+    DB_USER: str = os.environ.get("DB_USER")
+    DB_PASSWORD: str = os.environ.get("DB_PASSWORD")
+    DB_PORT: str = os.environ.get("DB_PORT")
+
+    ## create DB URL
+    DB_URL: str = (
+        f"postgresql+psycopg2://{DB_USER}:{parse.quote_plus(DB_PASSWORD)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+
+def settings():
+    """To return settings object"""
+    return Settings()
