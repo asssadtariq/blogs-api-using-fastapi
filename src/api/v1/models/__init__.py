6e9e48c4aacc4ca822a67815b2ced818ede4ab71
Asad Tariq <asadtariq.at66@gmail.com>
2024-09-18 12:37:44 +0500

    add: blog and blog category added in base file

--- src/api/v1/models/__init__.py
+++ src/api/v1/models/__init__.py
@@ -8,3 +8,5 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 from .user_model import User
+from .blog_category_model import BlogCategory
+from .blog_model import Blog
