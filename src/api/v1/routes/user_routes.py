8827392d75dd25244914de3ee01415bd91ee37b3
Asad Tariq <asadtariq.at66@gmail.com>
2024-09-25 14:13:45 +0500

    refactor and fix: fixed url bug and refactor code

--- src/api/v1/routes/user_routes.py
+++ src/api/v1/routes/user_routes.py
@@ -4,13 +4,13 @@ from fastapi import Depends
 from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 
-from src.api.v1.validators.user_validators import InValidatorUserAdd
 from src.api.v1.db.session import get_db
+from src.api.v1.validators.user_validators import InValidatorUserAdd
 
-router = APIRouter(prefix="/")
+router = APIRouter(prefix="/user")
 
 
 @router.post("/create_user", summary="")
 def create_user(user_detail: InValidatorUserAdd, db: Session = Depends(get_db)):
     """To create a new user"""
-    print("creating new user")
+    print("Creating new user /user")
