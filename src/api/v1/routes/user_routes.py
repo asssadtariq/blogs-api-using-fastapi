6316a55e7d683d3075b55cc74cf70002a9c307b9
Asad Tariq <asadtariq.at66@gmail.com>
2024-11-28 19:35:56 +0500

    update: updated user add route

--- src/api/v1/routes/user_routes.py
+++ src/api/v1/routes/user_routes.py
@@ -6,11 +6,21 @@ from sqlalchemy.orm import Session
 
 from src.api.v1.db.session import get_db
 from src.api.v1.validators.user_validators import InValidatorUserAdd
+from src.api.v1.core.security import validate_token
 
 router = APIRouter(prefix="/user")
 
 
 @router.post("/create_user", summary="")
-def create_user(user_detail: InValidatorUserAdd, db: Session = Depends(get_db)):
+def create_user(
+    user_detail: InValidatorUserAdd,
+    db: Session = Depends(get_db),
+    # _=Depends(validate_token),
+):
     """To create a new user"""
     print("Creating new user /user")
+
+    ## call the controller to add data
+    
+
+    ## return response
