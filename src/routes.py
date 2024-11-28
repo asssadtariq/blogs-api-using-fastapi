0c46bb6ad9a28e215f83c189146f7fe5032da5dc
Asad Tariq <asadtariq.at66@gmail.com>
2024-11-28 19:33:47 +0500

    add: auth route registered

--- src/routes.py
+++ src/routes.py
@@ -2,9 +2,11 @@
 
 from fastapi import FastAPI
 from src.api.v1.routes.user_routes import router as user_router
+from src.api.v1.routes.auth import router as auth_router
 
 
 def register_routes(app: FastAPI) -> None:
     """To register routes to our app"""
 
     app.include_router(router=user_router)
+    app.include_router(router=auth_router)
