6d51631ed76057885f8f8b6921d3896af90f0712
Asad Tariq <asadtariq.at66@gmail.com>
2025-01-09 17:53:45 +0500

    user routes file added for adding user related routes [in progress]

--- src/api/v1/routes/user_routes.py
+++ src/api/v1/routes/user_routes.py
@@ -5,22 +5,20 @@ from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 
 from src.api.v1.db.session import get_db
-from src.api.v1.validators.user_validators import InValidatorUserAdd
 from src.api.v1.core.security import validate_token
+from src.api.v1.controllers.user_controller import UserController
+from src.api.v1.validators.user_validators import InValidatorUserAdd
 
 router = APIRouter(prefix="/user")
 
 
 @router.post("/create_user", summary="")
 def create_user(
-    user_detail: InValidatorUserAdd,
+    user_details: InValidatorUserAdd,
     db: Session = Depends(get_db),
-    # _=Depends(validate_token),
 ):
     """To create a new user"""
-    print("Creating new user /user")
-
     ## call the controller to add data
-    
+    response = UserController(db=db).add_new_user(user_details=user_details)
 
     ## return response
