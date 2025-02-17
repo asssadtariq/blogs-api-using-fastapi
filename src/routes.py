33002a32dcbf1807d52fb0c3f30826c80c00f3c6
Asad Tariq <asadtariq.at66@gmail.com>
2025-02-17 17:46:45 +0500

    code refactoring

--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,12 @@
 """This file imports all routes and register it to our fast api app"""
 
 from fastapi import FastAPI
-from src.api.v1.routes.user_routes import router as user_router
 from src.api.v1.routes.auth import router as auth_router
+from src.api.v1.routes.user_routes import router as user_router
 
 
 def register_routes(app: FastAPI) -> None:
     """To register routes to our app"""
 
-    app.include_router(router=user_router)
-    app.include_router(router=auth_router)
+    app.include_router(router=auth_router, tags=["Auth"])
+    app.include_router(router=user_router, tags=["User"])
