4ca8906b3f5913c29d4eab809ed69b6780006f32
Asad Tariq <asadtariq.at66@gmail.com>
2025-02-17 17:05:48 +0500

    update: User service updated, adding user to database

--- src/api/v1/services/user_service.py
+++ src/api/v1/services/user_service.py
@@ -8,9 +8,11 @@ class UserService:
     def __init__(self, db: Session) -> None:
         self.db = db
 
-    def add_user(self, user_details: InValidatorUserAdd) -> User:
+    def add_user(
+        self, user_details: InValidatorUserAdd, do_commit: bool = True
+    ) -> User:
         user_obj: User = User(
-            username=user_details.user_id,
+            username=user_details.username,
             email=user_details.email,
             first_name=user_details.first_name,
             last_name=user_details.last_name,
@@ -18,3 +20,15 @@ class UserService:
             dob=user_details.dob,
             ver_key=user_details.ver_key,
         )
+
+        # add data in the db
+        self.db.add(user_obj)
+
+        # commit changes
+        if do_commit:
+            self.db.commit()
+
+        # refresh the data
+        self.db.refresh(user_obj)
+
+        return user_obj
